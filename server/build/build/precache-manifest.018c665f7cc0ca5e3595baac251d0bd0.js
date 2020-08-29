3819289f831bce2177b8271cdb7c99640761e7ea
MacBook <alinauroze@hotmail.com>
2020-08-29 16:21:05 +0500

    feat: project set up to run on https

---
+++ server/build/build/precache-manifest.018c665f7cc0ca5e3595baac251d0bd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0697ea22dafba2f6a02622de6ed9f9a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2d7ef3e1522ec5235f0",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fc03aed87d75d818f89c",
+    "url": "/static/js/2.09f1b02a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.09f1b02a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2d7ef3e1522ec5235f0",
+    "url": "/static/js/main.3c36247a.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ server/index.js
@@ -0,0 +1,17 @@
+const https = require('https');
+const fs = require('fs');
+const express = require('express');
+const app = express();
+
+app.use('/', express.static('./server/build'))
+
+const options = {
+  key: fs.readFileSync('./privkey.pem'),
+  cert: fs.readFileSync('./cert.pem')
+};
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/build/index.html")
+})
+
+https.createServer(options, app).listen(8443);
