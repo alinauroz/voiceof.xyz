689adb47e7bc3bdabbbfb582910e2c41f7dbfff0
MacBook <alinauroze@hotmail.com>
2020-08-31 18:34:17 +0500

    frontend cleaned up

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>VoiceOf.XYZ</h1>
-        <p>
-          Stay Anonymous Stay Safe 🛡
-        </p>
-        <a
-          className="App-link"
-          href="#coming_soon"
-          rel="noopener noreferrer"
-        >
-          PCS is ❤️
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor (props) {
+    super (props);
+  }
+  render () {
+    return <></>
+  }
 }
 
 export default App;
