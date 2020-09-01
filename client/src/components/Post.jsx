8603177a8d8f82dd0ebff6a6d08ad7957b7eed78
MacBook <alinauroze@hotmail.com>
2020-09-01 21:40:18 +0500

    style added

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -7,7 +7,8 @@ export default class Post extends React.Component {
     render () {
         return (
             <div className = 'main-container'>
-
+                <input type = 'text' placeholder = 'Email' className = 'input' />
+                <textarea className = 'text' placeholder = 'Your Message'></textarea>
             </div>
         )
     }
